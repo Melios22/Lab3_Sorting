3825f74b2046f6d6e65e0078c0f78a3dfb889366
Namronaldo2004 <thanhdoan0910@gmail.com>
2023-07-06 21:16:21 +0700

    Add file

---
+++ SOURCE/DataGenerator.h
@@ -0,0 +1,15 @@
+#ifndef DataGenerator_h
+#define DataGenerator_h
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <time.h>
+using namespace std;
+
+void GenerateRandomData(int a[], int n);
+void GenerateSortedData(int a[], int n);
+void GenerateReverseData(int a[], int n);
+void GenerateNearlySortedData(int a[], int n);
+void GenerateData(int a[], int n, int dataType);
+
+#endif
